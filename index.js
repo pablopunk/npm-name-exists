6c0bc87d2610bdb7bb01d71f965607976a24bf32
pablopunk <pablovarela182@gmail.com>
2017-12-10 21:27:02 +0000

    Little refactor

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
+'use strict'
+
 const { _builtinLibs } = require('repl')
 const { head } = require('got')
 
 const registry = 'https://registry.npmjs.org'
 
-// https://github.com/sindresorhus/npm-name/blob/master/index.js
-const request = name => {
+function checkRegistry (name) {
   return head(`${registry}/${name.toLowerCase()}`)
     .then(() => true)
-    .catch(err => {
-      if (err.statusCode === 404) {
-        return false
-      }
-    })
+    .catch(err => err.statusCode !== 404)
+}
+
+function isNodeLibrary (name) {
+  return _builtinLibs.includes(name)
 }
 
 module.exports = name => {
   if (typeof name !== 'string' || !name) {
     return false
   }
-  if (_builtinLibs.includes(name)) {
+  if (isNodeLibrary(name)) {
     return true
   }
 
-  return request(name)
+  return checkRegistry(name)
 }
